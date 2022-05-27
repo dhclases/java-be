59cfaed31cce70d197dac35a8ea4ca971d286903
rcastillo <castillo.randolfo2@gmail.com>
2022-05-26 23:20:27 -0300

    fix: ruta

--- clase 12/h2jdbc/src/com/digitalhouse/demo/util/LogGestor.java
+++ clase 12/h2jdbc/src/com/digitalhouse/demo/util/LogGestor.java
@@ -5,7 +5,7 @@ import org.apache.log4j.PropertyConfigurator;
 import java.io.File;
 
 public class LogGestor {
-    public static final String RUTA_ABSOLUTA_ARCHIVO_PROPIEDADES = "/home/rcastillo/ws/ws-digital/modulos/C12 Integracion/h2jdbc/src/log4j.properties";
+    public static final String RUTA_ABSOLUTA_ARCHIVO_PROPIEDADES = "coloco mi ruta aqui";
 
     public static void inicializar()  {
         File log4jfile = new File(RUTA_ABSOLUTA_ARCHIVO_PROPIEDADES);
